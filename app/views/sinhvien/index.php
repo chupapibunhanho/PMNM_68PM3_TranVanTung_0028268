8f68ed91d87daf707023e9fc723413f2583997f9
tran van tung <trantung09122005@gmail.com>
2026-06-02 20:46:46 +0700

    Thêm mới sinh viên

--- app/views/sinhvien/index.php
+++ app/views/sinhvien/index.php
@@ -3,32 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Danh sach sinh vien</title>
+    <title>danh sách sinh viên</title>
 </head>
 <body>
-    <h1>Danh sach sinh vien</h1>
-
-    <?php if (!empty($data['sinhviens'])): ?>
-        <table border="1" cellpadding="8" cellspacing="0">
-            <thead>
-                <tr>
-                    <?php foreach (array_keys($data['sinhviens'][0]) as $column): ?>
-                        <th><?php echo htmlspecialchars($column); ?></th>
-                    <?php endforeach; ?>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($data['sinhviens'] as $sinhvien): ?>
-                    <tr>
-                        <?php foreach ($sinhvien as $value): ?>
-                            <td><?php echo htmlspecialchars((string) $value); ?></td>
-                        <?php endforeach; ?>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-    <?php else: ?>
-        <p>Chua co du lieu sinh vien.</p>
-    <?php endif; ?>
 </body>
 </html>
